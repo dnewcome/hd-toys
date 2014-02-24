7bbe0341d7e69f3ae281d928cbf55b75c4726249
Michelle Steigerwalt <msteigerwalt@gmail.com>
2014-02-23 17:31:03 -0800

    Moved test isolation into a test util test rather than a random model.

--- tests/machine_test.py
+++ tests/machine_test.py
@@ -12,19 +12,3 @@ class TestMachine(util.IsolatedTestCase):
 		machine = Machine(name='Chainsaw')
 		machine.put()
 		self.assertEqual(Machine.all().count(), 1)
-
-	"""
-	" If we weren't using the IsolatedTestCase, this test would fail, since
-	" there would already be another item in the database.
-	"
-	" Go ahead, rename the setUp function inside of IsolatedTestCase and rerun
-	" the test to see what happens.
-	" 
-	" Hint: You can find the filename of IsolatedTestCase by looking at its
-	" import statement within this file.
-	"""
-	def test_test_isolation(self):
-		machine = Machine(name='Tesla Coil')
-		machine.put()
-		self.assertEqual(Machine.all().count(), 1)
-
---
+++ tests/util_test.py
@@ -0,0 +1,22 @@
+from main import app
+from models import Machine
+import util
+
+class TestUtil(util.IsolatedTestCase):
+
+	def test_isolation(self):
+		machine = Machine(name='Chainsaw')
+		machine.put()
+		self.assertEqual(Machine.all().count(), 1)
+
+	"""
+	" If we weren't using the IsolatedTestCase, this test would fail, since
+	" there would already be another item in the database.
+	"
+	" Go ahead, rename the setUp function inside of util.IsolatedTestCase and
+	" rerun the test to see what happens.
+	"""
+	def test_isolation_again(self):
+		machine = Machine(name='Tesla Coil')
+		machine.put()
+		self.assertEqual(Machine.all().count(), 1)
